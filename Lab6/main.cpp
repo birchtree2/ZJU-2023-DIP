0531680d33f6fc80af10f48345563e1546fe5f69
ypy <1980878786@qq.com>
2023-12-10 16:54:33 +0800

    test

--- Lab6/main.cpp
+++ Lab6/main.cpp
@@ -2,10 +2,15 @@
 #include<iostream>
 int main(){
     BMP in;
-    string fileName="2236";
+    string fileName="test";
     in.read("./img/"+fileName+".bmp");
     int space=80,color=80;
-    BMP out=bilateralFilter(in,15,space,color);
-    out.write("./img/bilateral_s"+to_string(space)+"_c"+to_string(color)+fileName+".bmp");
+    while(1){
+        cin>>space>>color;
+        BMP out=bilateralFilter(in,15,space,color);
+        out.write("./img/bilateral_s"+to_string(space)+"_c"+to_string(color)+
+        "_"+fileName+".bmp");//add param in filename for comparison
+    }
+    
 
 }
